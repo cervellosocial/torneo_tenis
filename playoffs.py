fc25e55f27c7400899f022bae904a9eabaf113ec
scifuentes <scifuentes@gmail.com>
2025-06-05 22:59:55 +0200

    Update playoffs.py

--- playoffs.py
+++ playoffs.py
@@ -9,57 +9,58 @@ def prepare_data():
   for group in playoffs :
     tabla_rondas = []
     len0 = len(group['ronda1']['partidos'])
-    print('len0',len0)
+    nCols = len0*2*2 - 1
     rondas = sorted(v for v in group if v.startswith('ronda'))+['final']
-    for j,ronda in enumerate(rondas):
-      row=[['',''] for v in range(len0*4)]
-      lenN = len(group[ronda]['partidos'])
-      k=int(len0/lenN)*4
-      o = int(k/4)-1 if j>0 else 0
-      print(ronda,lenN,'k',k,'o',o)
-      for i,entry in enumerate(group[ronda]['partidos']):
-        for ii in range(int(k/2)):
-          row[int(o+i*k+ii)]=('','class="playoff_match"')
-        row[int(o+i*k)]=(entry['jugador1'],'class="playoff_player1"')
-        row[int(o+i*k+k/2)]=(entry['jugador2'],'class="playoff_player2"')
-        row[int(o+i*k+k/4)]=(entry['resultado'],'class="playoff_match"')
-      tabla_rondas.append(row)
-  
-    row=[['',''] for v in range(len0*4)]
-    row[len0*2-1] = (group['ganador'],'class="playoff_player1"')
-    tabla_rondas.append(row)
-  
+    nRows = len(rondas)+1
+    for _ in range(nRows):
+      table.append([('','') for _ in range(nCols)])
+    print(len(tabla_rondas), len(tabla_rondas[0]))
+    
     headers = [group[ronda]['fecha'] for ronda in rondas]
-  
+    headers.append()
+    
+    for r,ronda in enumerate(rondas):
+      for i,partido in enumerate(group[ronda]['partidos']):
+        lenN = len(group[ronda]['partidos'])
+        k = int(len0/lenN)*4
+        o = int(k/4)-1 if r>0 else 0
+        c1 = o+i*k
+        c2 = c+k/2
+        for v in range(c1+1,c2):
+          tabla_rondas[r][v]=('','class="playoff_match"')
+        tabla_rondas[r][int(c)]=(partido['jugador1'],'class="playoff_player1"')
+        tabla_rondas[r][int(c2)]=(partido['jugador2'],'class="playoff_player2"')
+        tabla_rondas[r+1][int(c+k/4+1)]=(partido['resultado'],'')
+    tabla_rondas[-1][int(nCols/2)]=(group['ganador'],'')
+    tabla_rondas[-1][int(nCols/2+1)]=(group['final']['partidos'][0]['resultado'],'')
+    
+    headers34 =['' for _ in headers]
+    headers34[-2]='3er y 4to<br>'+group['final34']['fecha']
+    for _ in range(nRows):
+      tabla_rondas34.append([('','') for _ in range(4)])
+    tabla_rondas34[-2][0]=(group['final34']['partidos'][0]['jugador1'],'class="playoff_player1"')
+    tabla_rondas34[-2][3]=(group['final34']['partidos'][0]['jugador2'],'class="playoff_player2"')
+    tabla_rondas34[-1][2]=(group['ganador34'],'class="playoff_player1"')
+    tabla_rondas34[-1][3]=(group['final34']['partidos'][0]['resultado'],'class="playoff_player2"')
+    
     tabla_html=[]
     tabla_html.append('<table class="playoff_table">')
     tabla_html.append('<tr class="playoff_header">')
     tabla_html+=['<th>'+header+'</th>' for header in headers]
-    tabla_html.append('<th></th>')
     tabla_html.append('</tr>')
     for j,c in enumerate(tabla_rondas[0]):
       tabla_html.append('<tr>')
       for i,r in enumerate(tabla_rondas):
         tabla_html.append('<td '+tabla_rondas[i][j][1]+'>'+tabla_rondas[i][j][0]+'</td>')
       tabla_html.append('</tr>')
-      
     tabla_html.append('<tr class="playoff_header">')
-    tabla_html+=['<th> </th>' for _ in tabla_rondas]
-    tabla_html[-2]='<th>3er y 4to<br>'+group['final34']['fecha']+'</th>'
+    tabla_html+=['<th>'+header+'</th>' for header in headers34]
     tabla_html.append('</tr>')
-    tabla_html.append('<tr>')
-    tabla_html+=['<td> </td>' for _ in tabla_rondas]
-    tabla_html[-2]='<td class="playoff_player1">'+group['final34']['partidos'][0]['jugador1']+'</td>'
-    tabla_html.append('</tr>')
-    tabla_html.append('<tr>')
-    tabla_html+=['<td> </td>' for _ in tabla_rondas]
-    tabla_html[-2]='<td class="playoff_match">'+group['final34']['partidos'][0]['resultado']+'</td>'
-    tabla_html[-1]='<td class="playoff_player1">'+group['ganador34']+'</td>'
-    tabla_html.append('</tr>')
-    tabla_html.append('<tr>')
-    tabla_html+=['<td> </td>' for _ in tabla_rondas]
-    tabla_html[-2]='<td class="playoff_player2">'+group['final34']['partidos'][0]['jugador2']+'</td>'
-    tabla_html.append('</tr>')
+    for j,c in enumerate(tabla_rondas34[0]):
+      tabla_html.append('<tr>')
+      for i,r in enumerate(tabla_rondas34):
+        tabla_html.append('<td '+tabla_rondas[i][j][1]+'>'+tabla_rondas[i][j][0]+'</td>')
+      tabla_html.append('</tr>')
     tabla_html.append('</table>')
   
     out.append({
