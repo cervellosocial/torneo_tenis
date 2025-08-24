226d77044dc1d4ae30781fd1f5d0a856a2471a22
scifuentes <scifuentes@gmail.com>
2025-08-24 20:04:30 +0200

    Update torneos_grupos.py

--- torneos_grupos.py
+++ torneos_grupos.py
@@ -188,7 +188,7 @@ def generar_torneos():
         nombre_division = filename.replace('.json', '').replace('torneo','').replace('_',' ')
 
         # Añadir las tablas generadas al contexto
-        dheaders =['Posición','Jugador','Puntos','Partidos<br>Jugados','<br>Ganados','<br>Perdidos','Sets<br>Ganados','<br>Perdidos','Juegos<br>Ganados','<br>Perdidos','<br>Diferencia']
+        dheaders =['Posición','Jugador','Puntos','Partidos<br>Jugados','Partidos<br>Ganados','Partidos<br>Perdidos','Sets<br>Ganados','Sets<br>Perdidos','Juegos<br>Ganados','Juegos<br>Perdidos','Diferencia<br>Juegos']
         dheader = '<thead><tr><th>'+'</th><th>'.join(dheaders)+'</th></tr></thead>'
         tablas[nombre_division] = df_clasificacion.to_html(
             index=False, classes='table classification', header = False
@@ -209,6 +209,7 @@ def generar_torneos():
 
 if __name__ == '__main__':
     generar_torneos()
+
 
 
 
