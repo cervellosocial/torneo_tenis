aa79923f65feaf61dcfbda73bc282a6cd245c1fe
scifuentes <scifuentes@gmail.com>
2025-08-24 18:41:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from torneos import generar_torneos
+import torneos_clasico
+import torneos_grupos
 from playoffs import generate_playoffs
 
 if __name__ == '__main__':
-  generar_torneos()
+  torneos_grupos.generar_torneos()
+  torneos_clasico.generar_torneos()
   generate_playoffs()
