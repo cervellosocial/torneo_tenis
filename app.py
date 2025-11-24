3e0309b0dfb777ee93ab2c5220e9aebf5b187930
scifuentes <scifuentes@gmail.com>
2025-11-24 23:12:57 +0100

    limpieza structura antigua

--- app.py
+++
@@ -1,8 +0,0 @@
-import torneos_clasico
-import torneos_grupos
-from playoffs import generate_playoffs
-
-if __name__ == '__main__':
-  torneos_grupos.generar_torneos()
-  torneos_clasico.generar_torneos()
-  generate_playoffs()
